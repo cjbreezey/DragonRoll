f4704550c09e7fd97ed47784e28813a8121a6e15
Christopher Lee <chrisjlee250@gmail.com>
2020-10-14 10:45:33 -0700

    added navbar and styling

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -6,62 +6,129 @@ export default ({ user, logout }) => {
     const loggedOutNav = () => (
         <div className='loggedOut-nav-bar'>
             <div>
-                <a href="/" className='logo'>
-                    <img src={window.logo}></img>
-                    DragonRoll
+                <a href="/" >
+                    <img src={window.logo1} className='logo'></img>
+                    <span className='logo-text'>
+                    DRAGONROLL
+                    </span>
                 </a>
-            </div>
-            <div className='social-links'>
-                <a href="https://www.linkedin.com/in/christopher-j-lee/">
-                    <img className='linkedin' src={linkedin} />
-                </a>
-                <a href="https://github.com/cjbreezey/DragonRoll/">
-                    <img className='github' src={github} />
-                </a>
-            </div>
-            <div className='nav-bar-links'>
-                <NavLink to="/">
-                    <i className="fas fa-tv">
-                        <div className='nav-text'><span>Animes</span></div>
-                    </i>
-                </NavLink>
-                <NavLink to="/bookmarks">
-                    <i className="far fa-bookmark">
-                        <div className='nav-text'><span>Bookmarks</span></div>
-                    </i>
-                </NavLink>
-                <NavLink to="/login">
-                    <i className="far fa-user-circle">
-                        <div className='nav-text'><span>Log in</span></div>
-                    </i>
-                </NavLink>
+            <nav className='navbar-links'>
+                <ul>
+                    <li>
+                        <a><NavLink to="/">
+                            <i className="fas fa-tv">
+                                <div className='nav-text'><span>Animes</span></div>
+                            </i>
+                        </NavLink></a>
+                    </li>
+                    <li>
+                        <NavLink to="/login">
+                            <i className="far fa-bookmark">
+                                <div className='nav-text'><span>Bookmarks</span></div>
+                            </i>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/login">
+                            <i className="far fa-user-circle">
+                                <div className='nav-text'><span>Log in</span></div>
+                            </i>
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+            <nav className='social-links'>
+                <ul>
+                    <li>
+                        <a href="https://www.linkedin.com/in/christopher-j-lee/">
+                            <img className='linkedin' src={linkedin} />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/cjbreezey/DragonRoll/">
+                            <img className='github' src={github} />
+                        </a>
+                    </li>
+                </ul>
+            </nav>
             </div>
         </div>
     )
 
     const loggedInNav = () => (
-        <div className='loggedIn-nav-bar'>
+        <div className='loggedOut-nav-bar'>
             <div>
-                <a href="/">
-                    <img className='logo' src={window.logo}></img>
-                    DragonRoll
+                <a href="/" >
+                    <img src={window.logo1} className='logo'></img>
+                    <span className='logo-text'>
+                        DRAGONROLL
+                    </span>
                 </a>
-            </div>
-            <div className='social-links'>
-                <a href="https://www.linkedin.com/in/christopher-j-lee/">
-                    <img className='linkedin' src={linkedin} />
-                </a>
-                <a href="https://github.com/cjbreezey/DragonRoll/">
-                    <img className='github' src={github} />
-                </a>
-                <input className='search-bar' type="text" />
-            </div>
-            <div className='nav-bar-links'>
-                <NavLink to="/">Animes</NavLink>
-                <button className='logout' onClick={logout}><a>Log Out</a></button>
-                <NavLink to="/login">Log In</NavLink>
+            <nav className='navbar-links'>
+                <ul>
+                    <li>
+                        <a><NavLink to="/">
+                            <i className="fas fa-tv">
+                                <div className='nav-text'><span>Animes</span></div>
+                            </i>
+                        </NavLink></a>
+                    </li>
+                    <li>
+                        <NavLink to="/login">
+                            <i className="far fa-bookmark">
+                                <div className='nav-text'><span>Bookmarks</span></div>
+                            </i>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/login">
+                            <i className="far fa-user-circle">
+                                <div className='nav-text' onClick={logout}><span>Log Out</span></div>
+                            </i>
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+            <nav className='social-links'>
+                <ul>
+                    <li>
+                        <a href="https://www.linkedin.com/in/christopher-j-lee/">
+                            <img className='linkedin' src={linkedin} />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/cjbreezey/DragonRoll/">
+                            <img className='github' src={github} />
+                        </a>
+                    </li>
+                </ul>
+            </nav>
             </div>
         </div>
+
+
+        // <div className='loggedIn-nav-bar'>
+        //     <div>
+        //         <a href="/">
+        //             <img className='logo' src={window.logo}></img>
+        //             {/* DragonRoll */}
+        //         </a>
+        //     </div>
+        //     <div className='social-links'>
+        //         <a href="https://www.linkedin.com/in/christopher-j-lee/">
+        //             <img className='linkedin' src={linkedin} />
+        //         </a>
+        //         <a href="https://github.com/cjbreezey/DragonRoll/">
+        //             <img className='github' src={github} />
+        //         </a>
+        //         <input className='search-bar' type="text" />
+        //     </div>
+        //     <div className='nav-bar-links'>
+        //         <NavLink to="/">Animes</NavLink>
+        //         <button className='logout' onClick={logout}><a>Log Out</a></button>
+        //         <NavLink to="/login">Log In</NavLink>
+        //     </div>
+        // </div>
     )
     
     return user ? loggedInNav() :loggedOutNav()
