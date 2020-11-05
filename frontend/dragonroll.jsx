826c5eb732696735706476b64ce10abc866c1d74
Christopher Lee <chrisjlee250@gmail.com>
2020-11-04 20:16:47 -0800

    clean unnecessary code and debuggers

--- frontend/dragonroll.jsx
+++ frontend/dragonroll.jsx
@@ -2,10 +2,7 @@ import ReactGA from 'react-ga'
 import React from 'react';
 import ReactDom from 'react-dom'
 import configureStore from './store/store';
-import {login, logout, signup} from './actions/session_actions'
 import Root from './components/root'
-import { fetchAnime, fetchAnimes } from '../frontend/actions/anime_actions';
-import { createBookmarkedAnime } from './actions/bookmarked_anime_actions';
 
 function initializeReactGA() {
     ReactGA.initialize('G-YCRXFMQNYL');
@@ -30,15 +27,4 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     const root = document.getElementById('root');
     ReactDom.render(<Root store={store} />, root);
-
-    // window testing
-    window.store = store;
-    window.dispatch = store.dispatch;
-    window.login = login;
-    window.signup = signup;
-    window.logout = logout;
-    window.fetchAnimes = fetchAnimes
-    window.fetchAnime = fetchAnime
-    window.createBookmarkedAnime = createBookmarkedAnime
-    // window testing
 })
